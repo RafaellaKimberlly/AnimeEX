f2dbe537819f1c9e73b915eae05dbe426496ab27
RafaellaKimberlly <rafaella.souza@bandtec.com.br>
2021-05-10 10:19:53 -0300

    Atualizado dia 10/05

---
+++ html/js-home.js
@@ -0,0 +1,5 @@
+function menu(){
+    document.getElementById("menu-bar").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+    }
